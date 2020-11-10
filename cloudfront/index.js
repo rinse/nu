93120484f817c54214011866c643310dcc05a357
rinse <rinse@neko2.net>
2020-11-10 15:29:50 +0900

    add lambda@edge

---
+++ cloudfront/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports.handler = (event, context, callback) => {
+    const response = event.Records[0].cf.response;
+    const headers = response.headers;
+    headers['x-serverless-time'] = [{ key: 'x-serverless-time', value: Date.now().toString() }];
+    return callback(null, response);
+};
